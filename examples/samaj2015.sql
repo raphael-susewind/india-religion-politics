bf20124852bb8d9099e98ca0c6f51d8c4ac0cbd4
Raphael Susewind <mail@raphael-susewind.de>
2017-01-16 10:55:19 +0100

    Make sure that the map generation example uses station_ids rather than booth_ids for a smoother map (which is what it actually does...)

--- examples/samaj2015.sql
+++ examples/samaj2015.sql
@@ -2,10 +2,12 @@
 .headers on
 .once samaj2015.csv
 SELECT 
-upgis.latitude 'latitude',
-upgis.longitude 'longitude',
-uprolls2014.muslim_percent_14 'muslim_percent'
+avg(upgis.latitude) 'latitude',
+avg(upgis.longitude) 'longitude',
+avg(uprolls2014.muslim_percent_14) 'muslim_percent'
 FROM upgis 
-JOIN uprolls2014 ON upgis.ac_id_09 = uprolls2014.ac_id_09 AND upgis.booth_id_14 = uprolls2014.booth_id_14
+LEFT JOIN uprolls2014 ON upgis.ac_id_09 = uprolls2014.ac_id_09 AND upgis.booth_id_14 = uprolls2014.booth_id_14
+LEFT JOIN upid ON upgis.ac_id_09 = upid.ac_id_09 AND upgis.booth_id_14 = upid.booth_id_14
 WHERE upgis.ac_id_09 BETWEEN 168 AND 176
+GROUP BY upid.station_id_14
 ;
